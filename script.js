01146588cc2b90ec6107484c3cd68219eb8f0756
SoumyaMakkar <soumyamakkar123@gmail.com>
2024-10-13 18:16:56 +0530

    new changes

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function valueSetters(){
+    gsap.set("#nav a",{y:"-100%",opacity:0});
+    gsap.set("#home span .child",{y:"100%"});
+    gsap.set("#home .row img",{opacity:0});
+
+    document.querySelectorAll("#Visual>g").forEach(function (e) {
+        const paths = e.querySelectorAll('path, polyline');
+        paths.forEach(function(path) {
+            const length = path.getTotalLength();
+            path.style.strokeDasharray = length + 'px';
+            path.style.strokeDashoffset = length + 'px';
+        });
+    });
+}
+
+
 function revealToSpan(){
     document.querySelectorAll(".reveal").forEach(function(elem){
     console.log("script is workng");
@@ -15,12 +31,11 @@ function revealToSpan(){
 });
 }
 
-revealToSpan();
+function loaderAnimation(){
+    var tl=gsap.timeline();
 
-var tl=gsap.timeline();
 
-
-tl.from(".child span",{
+tl.from("#loader .child span",{
     x:150,
     opacity:0,
     delay:.3,
@@ -28,13 +43,13 @@ tl.from(".child span",{
     stagger:.1,
     ease:Power3.easeInOut,
 })
-.to(".parent .child span",{
+.to("#loader .parent .child span",{
     y: "-100%",
     stagger:0.1,
     duration: 0.6,
     ease: Expo.easeInOut
 })
-.to(".parent .child",{
+.to("#loader .parent .child",{
     y: "-100%",
     stagger:0.1,
     delay:-0.8,
@@ -58,4 +73,61 @@ tl.from(".child span",{
     delay:-0.7,
     duration:1,
     ease:Expo.easeOut,
+    onComplete:function(){
+        animateHomepage();
+    }
 })
+}
+
+function animateSvg() {
+    
+
+    gsap.to("#Visual>g>g>path:not(#dot_path), #Visual>g>g>polyline", {
+        strokeDashoffset: 0,
+        duration: 1,
+        ease: "Power2.inOut",
+        stagger:0.2,
+        onComplete: function() {
+            // After the letters are done, animate the dot
+            const dotPath = document.querySelector('#dot_path');
+            const dotLength = dotPath.getTotalLength();
+            dotPath.style.strokeDasharray = dotLength + 'px';
+            dotPath.style.strokeDashoffset = dotLength + 'px';
+            
+            // Animate the dot to appear
+            gsap.to(dotPath, {
+                strokeDashoffset: 0,
+                duration: 1,
+                ease: Expo.easeInOut,
+                delay: 0.1 
+            });
+        }
+    });
+}
+
+function animateHomepage(){
+
+    var tl=gsap.timeline();
+    tl.to("#nav a",{
+        y:0,
+        opacity:1,
+        stagger:0.05,
+        ease:Expo.easeInOut
+    }).to("#home span .child",{
+        y:0,
+        stagger:0.1,
+        ease:Expo.easeInOut,
+        duration:1,
+    }).to("#home .row img",{
+        opacity:1,
+        ease:Expo.easeInOut,
+        onComplete:function(){
+            animateSvg();
+        }
+    })
+}
+
+revealToSpan();
+valueSetters();
+loaderAnimation();
+
